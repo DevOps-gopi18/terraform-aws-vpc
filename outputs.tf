cc3d3e9af789d29441a833778842e2837ad7bd2c
gopih <gopi47590@gmail.com>
2025-04-13 12:26:20 +0530

    ssm parameter store

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,18 @@
   value       = aws_subnet.public
 } */
 
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private[*].id
+}
+
+output "databse_subnet_id" {
+  value = aws_subnet.database[*].id
+}
